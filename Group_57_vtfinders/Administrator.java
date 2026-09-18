135b27fb796c92c52793960c216ba966aed1ccc7
Phani Kathu <phanikathu2007@gmail.com>
2026-09-18 10:57:31 -0400

    Update Administrator.java

--- Group_57_vtfinders/Administrator.java
+++ Group_57_vtfinders/Administrator.java
@@ -4,4 +4,12 @@ public class Administrator
     private String username;
     private String password;
     private LostItemDatabase database;
+
+    public Administrator(String adminId, String username,
+        String password, ListItemDatabase database){
+            this.adminId = adminId;
+            this.username = username;
+            this.password = password;
+            this.database = database
+        }
 }
