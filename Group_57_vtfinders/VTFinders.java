2f893c91d3f279e6c6b9f3e88998967ee8a98a0c
Phani Kathu <phanikathu2007@gmail.com>
2026-09-23 12:45:14 -0400

    adding the different options in the vtfinders class

--- Group_57_vtfinders/VTFinders.java
+++ Group_57_vtfinders/VTFinders.java
@@ -22,17 +22,100 @@ public class VTFinders
 
             if (choice.equals("1"))
             {
-                //report items
+                System.out.print("Enter item name:");
+                String name = scanner.nextLine();
+
+                System.out.print("Enter Description:");
+                String description = scanner.nextLine();
+
+                System.out.print("Enter Location:");
+                String location = scanner.nextLine();
+
+                System.out.print("Enter date found:");
+                String date = scanner.nextLine();
+
+                System.out.println("Choose a category:");
+                System.out.println("A: School Supplies");
+                System.out.println("B: Electronics");
+                System.out.println("C: Clothing");
+                System.out.println("D: Personal Items");
+                System.out.println("E: Miscellaneous");
+
+                String categoryChoice = scanner.nextLine();
+                String category = "";
+
+                if (categoryChoice.equals("A"))
+                {
+                    category = "School Supplies";
+                }
+
+                else if (categoryChoice.equals("B"))
+                {
+                    category = "Electronics";
+                }
+
+                else if (categoryChoice.equals("C"))
+                {
+                    category = "Clothing";
+                }
+
+                else if (categoryChoice.equals("D"))
+                {
+                    category = "Personal Items";
+                }
+
+                else if (categoryChoice.equals("E"))
+                {
+                    category = "Miscellaneous";
+                }
+
+                client.reportItem(name, description, location, date, category);
+
             }
-            if (choice.equals("2"))
+            else if (choice.equals("2"))
             {
-                //brows items
+                System.out.println("Choose a category:");
+                System.out.println("A: School Supplies");
+                System.out.println("B: Electronics");
+                System.out.println("C: Clothing");
+                System.out.println("D: Personal Items");
+                System.out.println("E: Miscellaneous");
+
+                String categoryChoice = scanner.nextLine();
+                String category = "";
+
+                if (categoryChoice.equals("A"))
+                {
+                    category = "School Supplies";
+                }
+
+                else if (categoryChoice.equals("B"))
+                {
+                    category = "Electronics";
+                }
+
+                else if (categoryChoice.equals("C"))
+                {
+                    category = "Clothing";
+                }
+
+                else if (categoryChoice.equals("D"))
+                {
+                    category = "Personal Items";
+                }
+
+                else if (categoryChoice.equals("E"))
+                {
+                    category = "Miscellaneous";
+                }
+
+                client.browseItems(category);
             }
-            if (choice.equals("3"))
+            else if (choice.equals("3"))
             {
                 //admin
             }
-            if (choice.equals("4"))
+            else if (choice.equals("4"))
             {
                 running = false;
             }   
