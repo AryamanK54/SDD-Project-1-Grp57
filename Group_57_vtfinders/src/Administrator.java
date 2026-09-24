9afc6307288ec6767d6fc52ddb97ff296dc93646
Phani Kathu <phanikathu2007@gmail.com>
2026-09-24 17:04:27 -0400

    getting rid of unneeded things

--- Group_57_vtfinders/src/Administrator.java
+++ Group_57_vtfinders/src/Administrator.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 
 public class Administrator
 {
-    private String adminId;
     private String username;
     private String password;
     private LostItemDatabase database;
@@ -25,7 +24,6 @@ public class Administrator
 
     public Administrator(String adminId, String username,
         String password, LostItemDatabase database){
-            this.adminId = adminId;
             this.username = username;
             this.password = password;
             this.database = database;
