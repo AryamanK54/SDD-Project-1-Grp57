dcb81abcbb4ad25e0971856bd12dec8d21c895eb
Phani Kathu <phanikathu2007@gmail.com>
2026-09-24 09:53:13 -0400

    Writing the test methods for viewAllItems and RemoveItem

--- Group_57_vtfinders/src/AdministratorTest.java
+++ Group_57_vtfinders/src/AdministratorTest.java
@@ -18,4 +18,25 @@ public class AdministratorTest extends TestCase
         assertFalse(administrator.authentication("adminUsername", "wrongPassword"));
         assertFalse(administrator.authentication("wrongUsername", "wrongPassword"));
     }
+
+    public void testViewAllItems()
+    {
+        LostItem item1 = new LostItem("1", "Backpack", "Black backpack", "library",
+            "Tuesday", "Personal Items");
+        database.addItem(item1);
+
+        assertEquals(1, administrator.viewAllItems().size());
+        assertEquals(item1, administrator.viewAllItems().get(0));
+    }
+    
+    public void testRemoveItem()
+    {
+        LostItem item1 = new LostItem("1", "Backpack", "Black backpack", "library",
+            "Tuesday", "Personal Items");
+        database.addItem(item1);
+        administrator.removeItem("1");
+        assertEquals(0, administrator.viewAllItems().size());
+    }
+
 }
+
