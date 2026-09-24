9e737c039746634459e92e650176091a561e97bd
Phani Kathu <phanikathu2007@gmail.com>
2026-09-24 17:05:13 -0400

    adding back the adminId

--- Group_57_vtfinders/src/Administrator.java
+++ Group_57_vtfinders/src/Administrator.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class Administrator
 {
+    private String adminId;
     private String username;
     private String password;
     private LostItemDatabase database;
@@ -24,6 +25,7 @@ public class Administrator
 
     public Administrator(String adminId, String username,
         String password, LostItemDatabase database){
+            this.adminId = adminId;
             this.username = username;
             this.password = password;
             this.database = database;
