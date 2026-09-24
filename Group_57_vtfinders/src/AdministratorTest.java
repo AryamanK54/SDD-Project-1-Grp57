a6483bea0d863de73af7f7e845ac985f1ca11e5c
Phani Kathu <phanikathu2007@gmail.com>
2026-09-24 09:38:24 -0400

    Testing the authentication

--- Group_57_vtfinders/src/AdministratorTest.java
+++ Group_57_vtfinders/src/AdministratorTest.java
@@ -10,4 +10,12 @@ public class AdministratorTest extends TestCase
         database = new LostItemDatabase();
         administrator = new Administrator("admin1", "adminUsername", "f21", database);
     }
+
+    public void testAuthentication()
+    {
+        assertTrue(administrator.authentication("adminUsername", "f21"));
+        assertFalse(administrator.authentication("wrongUsername", "f21"));
+        assertFalse(administrator.authentication("adminUsername", "wrongPassword"));
+        assertFalse(administrator.authentication("wrongUsername", "wrongPassword"));
+    }
 }
